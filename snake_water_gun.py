1cd71bf80366055e87fe1435edd1c03a64dcbd1f
Anil Shrestha <68494220+anil0403@users.noreply.github.com>
2021-07-17 13:34:35 +0545

    Add files via upload

---
+++ snake_water_gun.py
@@ -0,0 +1,48 @@
+# Author : Anil Shrestha
+# its only for educational purpose
+# its a snake water gun game coded in python
+# any inerested begineer programmer are allowed  modify the codes
+import random
+
+def game(com, you):
+    if com == "s":
+        if you == "w":
+            return False
+        elif you == "g":
+            return True
+    elif com == "w":
+        if you == "s":
+            return True
+        elif you == "g":
+            return False
+    elif com == "g":
+        if you == "w":
+            return True
+        elif you == "s":
+            return False
+    elif com == you:
+        return None
+
+
+# Two players a(computer) and b
+print("Snake Water or Gun game: ")
+print("Computer's Turn: Snake(1) Water(2) or Gun(3) ?")
+a = random.randrange(1, 4)
+if a == 1:
+    a = "s"
+elif a == 2:
+    a = "w"
+elif a == 3:
+    a = "g"
+
+b = input(" Your Turn: Snake(s) Water(w) or Gun(g) ? \n")
+stat = game(a, b)
+if stat == None:
+    print("Tie!")
+elif stat == True:
+    print("you win!")
+else:
+    print("you lost!")
+
+print(f"Computer choose = {a}")
+print(f"You Choose = {b}")
