671858f641311761f4ec1c5865285a20ccaa7b6b
Anil Shrestha <anilshrestha0403.as@gmail.com>
2021-07-17 13:59:52 +0545

    Update snake_water_gun.py

--- snake_water_gun.py
+++ snake_water_gun.py
@@ -3,6 +3,8 @@
 # its a snake water gun game coded in python
 # any inerested begineer programmer are allowed  modify the codes
 import random
+# ramdom helps to pick random selection
+
 
 def game(com, you):
     if com == "s":
@@ -27,6 +29,7 @@ def game(com, you):
 # Two players a(computer) and b
 print("Snake Water or Gun game: ")
 print("Computer's Turn: Snake(1) Water(2) or Gun(3) ?")
+# computer selection
 a = random.randrange(1, 4)
 if a == 1:
     a = "s"
@@ -34,9 +37,11 @@ elif a == 2:
     a = "w"
 elif a == 3:
     a = "g"
-
+# user input(selection)
 b = input(" Your Turn: Snake(s) Water(w) or Gun(g) ? \n")
+
 stat = game(a, b)
+# analysing status A/C to value received from game function
 if stat == None:
     print("Tie!")
 elif stat == True:
@@ -44,5 +49,6 @@ elif stat == True:
 else:
     print("you lost!")
 
+# printing selections
 print(f"Computer choose = {a}")
 print(f"You Choose = {b}")
